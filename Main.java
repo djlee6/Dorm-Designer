ca8460b11845d702360b41ac87d7f5086536c1ae
djlee6 <38513429+djlee6@users.noreply.github.com>
2018-07-16 13:59:16 -0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 //////////////////// ALL ASSIGNMENTS INCLUDE THIS SECTION /////////////////////
 //
 // Title:           DormDesigner3000
@@ -33,18 +35,12 @@
 /////////////////////////////// 80 COLUMNS WIDE ///////////////////////////////
 public class Main {
     
+    private ArrayList<DormGUI> guiObjects;
     private PApplet processing; //instance fields of class main
     private PImage backgroundImage;
-    private Furniture furniture[];
     private int dragBedIndex;
-    private CreateFurnitureButton bedButton;
-    private CreateFurnitureButton sofaButton;
-    private CreateFurnitureButton dresserButton;
-    private CreateFurnitureButton deskButton;
-    private CreateFurnitureButton sinkButton;
-    private SaveButton saveButton;
-    private LoadButton loadButton;
-    private ClearButton clearButton;
+    private final static int MAX_LOAD_FURNITURE = 100; 
+    private CreateFurnitureButton button;
     
     public static void main(String[] args) {
         
@@ -59,16 +55,16 @@ public class Main {
                 
         dragBedIndex = -1;
         
-        furniture = new Furniture[6];
-        
-        bedButton = new CreateFurnitureButton("Bed", 50, 24, processing);
-        sofaButton = new CreateFurnitureButton("Sofa", 150, 24, processing);
-        dresserButton = new CreateFurnitureButton("Dresser", 250, 24, processing);
-        deskButton = new CreateFurnitureButton("Desk", 350, 24, processing);
-        sinkButton = new CreateFurnitureButton("Sink", 450, 24, processing);
-        clearButton = new ClearButton(550, 24, processing);
-        saveButton = new SaveButton(650, 24, processing);
-        loadButton = new LoadButton(750, 24, processing);
+        guiObjects = new ArrayList<DormGUI>();
+
+        guiObjects.add(new CreateFurnitureButton("Bed", 50, 24, processing));
+        guiObjects.add(new CreateFurnitureButton("Sofa", 150, 24, processing));
+        guiObjects.add(new CreateFurnitureButton("Dresser", 250, 24, processing));
+        guiObjects.add(new CreateFurnitureButton("Desk", 350, 24, processing));
+        guiObjects.add(new CreateFurnitureButton("Sink", 450, 24, processing));
+        guiObjects.add(new ClearButton(550, 24, processing));
+        guiObjects.add(new SaveButton(650, 24, processing));
+        guiObjects.add(new LoadButton(750, 24, processing));
     }
     
     public void update() { //update method displays current furniture positions
@@ -77,79 +73,78 @@ public class Main {
         
         processing.image(backgroundImage, processing.width/2, processing.height/2); //places the background image in center of window
         
-        bedButton.update();
-        sofaButton.update();
-        dresserButton.update();
-        deskButton.update();
-        sinkButton.update();
-        clearButton.update();
-        saveButton.update();
-        loadButton.update();
-        
-        for(int i = 0; i < furniture.length; i++) { //updates every furniture object to a new position
-            if(dragBedIndex >= 0) {
-                if(furniture[dragBedIndex] != null) {
-                    furniture[dragBedIndex].update();
-                }
-            }
-            if(furniture[i] != null) {
-                furniture[i].update();
-            }
+        for(int j = 0; j < guiObjects.size(); j++) {
+            guiObjects.get(j).update();
         }
+    }
+    
+    /**
+     * This method creates a new Furniture[] for the old mouseDown() methods
+     * to make use of.  It does so by copying all Furniture references from
+     * this.guiObjects into a temporary array of size MAX_LOAD_FURNITURE.
+     * @return that array of Furniture references.
+     */
+    private Furniture[] extractFurnitureFromGUIObjects() {
+        Furniture[] furniture = new Furniture[MAX_LOAD_FURNITURE];
+        int nextFreeIndex = 0;
+        for(int i=0;i<guiObjects.size() && nextFreeIndex < furniture.length;i++)
+            if(guiObjects.get(i) instanceof Furniture)
+                furniture[nextFreeIndex++] = (Furniture)guiObjects.get(i);        
+        return furniture;        
+    }  
+    
+    /**
+     * This method first removes all Furniture references from this.guiObjects,
+     * and then adds back in all of the non-null references from it's parameter.
+     * @param furniture contains the only furniture that will be left in 
+     *   this.guiObjects after this method is invoked (null references ignored).
+     */
+    private void replaceFurnitureInGUIObjects(Furniture[] furniture) {
+        for(int i=0;i<guiObjects.size();i++)
+            if(guiObjects.get(i) instanceof Furniture)
+                guiObjects.remove(i--);
+        for(int i=0;i<furniture.length;i++)
+            if(furniture[i] != null)
+                guiObjects.add(furniture[i]);
     }
     
     public void mouseDown() { //method checks where the mouse is clicked
         
-        for(int i = 0; i < furniture.length; i++) {
-            if(furniture[i] == null) {
-                if(bedButton.isMouseOver()) {//if the mouse clicks the bedButton a new bed is created
-                    furniture[i] = bedButton.mouseDown();
-                    return;
-                }
-                if(sofaButton.isMouseOver()) {//if the mouse clicks the sofabutton a new sofa is created
-                    furniture[i] = sofaButton.mouseDown();
-                    return;
-                }
-                if(dresserButton.isMouseOver()) {
-                    furniture[i] = dresserButton.mouseDown();
-                    return;
-                }
-                if(deskButton.isMouseOver()) {
-                    furniture[i] = deskButton.mouseDown();
-                    return;
-                }
-                if(sinkButton.isMouseOver()) {
-                    furniture[i] = sinkButton.mouseDown();
-                    return;
-                }
-            }
-            if(furniture[i] != null) {//if a furniture object is clicked it can be moved
-                furniture[i].mouseDown(furniture);
-                dragBedIndex = i;
-            }
-        }
-        if(clearButton.isMouseOver()) {
-            if(clearButton.mouseDown()) {
-                for(int j = 0; j < furniture.length; j++) {
-                    furniture[j] = null;
+            Furniture[] furniture = extractFurnitureFromGUIObjects();
+            for(int j = 0; j < guiObjects.size(); j++) {
+                if(guiObjects.get(j).isMouseOver()) {
+                    if(guiObjects.get(j) instanceof ClearButton) {
+                        furniture = null;
+                        break;
+                    }
+                    if(guiObjects.get(j) instanceof SaveButton || guiObjects.get(j) instanceof LoadButton) {
+                        guiObjects.get(j).mouseDown(furniture);
+                    }
+                    else {
+                        for(int i = 0; i < furniture.length; i++) {
+                            if(furniture[i] == null) {
+                                button = (CreateFurnitureButton)guiObjects.get(j);
+                                furniture[i] = (Furniture)button.mouseDown();
+                                break;
+                            }
+                            else {
+                                furniture[i].mouseDown(furniture);
+                                
+                            }
+                        }
+                    }
                 }
             }
-        }
-        if(saveButton.isMouseOver()) {
-            saveButton.mouseDown(furniture);
-        }
-        if(loadButton.isMouseOver()) {
-            loadButton.mouseDown(furniture);
-        }
+               // Your code comes here to explore guiObjects ArrayList and call the related mouseDown(furniture) method
+               // if the element is pressed
+        replaceFurnitureInGUIObjects(furniture);
     }
+    
     
     public void mouseUp() { //if the mouse is lifted up the furniture objects cannot be moved
         
-        for(int i = 0; i < furniture.length; i++) {
-            if(furniture[i] != null) {
-                dragBedIndex = -1;
-                furniture[i].mouseUp();
-            }
+        for(int j = 0; j < guiObjects.size(); j++) {
+            guiObjects.get(j).mouseUp();
         }
     }
     
